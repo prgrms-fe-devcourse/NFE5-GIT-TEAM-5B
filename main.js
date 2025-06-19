8aebb97512dd7e69c0394106b916c30607064f42
forntend-developer <ejdeveloper0305@gmail.com>
2025-06-19 15:57:36 +0900

    네비, 인트로 작업 업로드

--- main.js
+++ main.js
@@ -1,4 +1,39 @@
-import {  } from './js/index.js';
+import {} from "./js/index.js";
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const toggle = $(".nav_btn");
+const nav = $(".nav");
+const gnb = $(".gnb");
+const stripes = $(".stripes");
+const links = $$(".nav a");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("visible");
+  toggle.classList.toggle("visible");
+  stripes.classList.toggle("visible");
+  gnb.classList.toggle("hidden");
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.closest(".nav a")) {
+    nav.classList.remove("visible");
+    toggle.classList.remove("visible");
+    stripes.classList.remove("visible");
+    gnb.classList.remove("hidden");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY || window.pageYOffset;
+  if (scrollY > 800) {
+    gnb.classList.add("dark");
+    toggle.classList.add("dark");
+  } else {
+    gnb.classList.remove("dark");
+    toggle.classList.remove("dark");
+  }
+});
 
 const split = new SplitText(".main_visual01 h2", { type: "chars" });
 gsap.from(split.chars, {
@@ -12,8 +47,8 @@ gsap.from(split.chars, {
 
 gsap.registerPlugin(ScrollTrigger);
 
-const visualWrapper = document.querySelector(".main_visual_wrapper");
-const visualCount = document.querySelectorAll(".main_visual").length;
+const visualWrapper = $(".main_visual_wrapper");
+const visualCount = $$(".main_visual").length;
 const wrapperWidth = visualCount * window.innerWidth;
 
 visualWrapper.style.width = wrapperWidth + "px";
